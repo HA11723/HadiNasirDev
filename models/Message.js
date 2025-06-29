1975c7a2995afad9ec440847653259637e1c845d
Hadi Nasir <hadinasser067@gmail.com>
2025-06-29 15:50:16 -0700

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,12 +1,1 @@
-const mongoose = require('mongoose');
 
-const messageSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    subject: String,
-    message: String,
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Message', messageSchema);
